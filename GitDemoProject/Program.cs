a01f6f7d5192b0060209094d65bfc2c67906eb0b
Aynamik4 <violintailgut@gmail.com>
2021-01-31 16:16:02 +0100

    Just some additional code

--- GitDemoProject/Program.cs
+++ GitDemoProject/Program.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿// 1. Compiler errors.
+// 2. Run-time errors.
+// 3. Logical errors (intermittant).
+
+using System;
 
 namespace GitDemoProject
 {
@@ -6,8 +10,39 @@ namespace GitDemoProject
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("Git");
+            Console.Write("Vänligen mata in en nämnare: ");
+            string keyboardInput = Console.ReadLine();
+            int nämnare = 0;
+
+            try
+            {
+                nämnare = int.Parse(keyboardInput);
+                int kvot = 20 / nämnare;
+                Console.WriteLine($"{20} / {nämnare} = {kvot}");
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Du kan inte dividera med talet 0.");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Felaktig inmatning!");
+                throw;
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine($"Talet måste vara ett tal mellan {int.MinValue} och {int.MaxValue}");
+            }
+            catch(Exception)
+            {
+                Console.WriteLine("Det har uppstått ett oväntat fel.");
+            }
+            finally
+            {
+                Console.WriteLine("Denna kodrad kommer ALLTID att köra oavsett om vi får ett exception eller inte.");
+            }
+
+            Console.WriteLine("Nu fortsätter programmet att köra!");
         }
     }
 }
